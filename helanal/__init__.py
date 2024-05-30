509273045e349e1570b2edb47402d52ea71a74f9
fiona-naughton <fiona@mdanalysis.org>
2024-05-29 18:22:20 -0700

    redo copy initial+tests from core

--- helanal/__init__.py
+++ helanal/__init__.py
@@ -4,6 +4,9 @@ A package to characterise the geometry of protein helicies.
 """
 
 # Add imports here
+from .helanal import HELANAL
+from .helanal import local_screw_angles, helix_analysis, vector_of_best_fit
+
 from importlib.metadata import version
 
 __version__ = version("helanal")
